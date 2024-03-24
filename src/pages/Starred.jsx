168004dd4d628e44ab298896789d3e848c3840ea
Pranav Mange <pranavmange2015@gmail.com>
2024-03-24 12:59:06 +0530

    search box updated

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,9 +1,32 @@
+import { getShowByIds } from '../api/tvmaze';
 import { useStarredShows } from '../lib/useStarredShows';
+import { useQuery } from '@tanstack/react-query';
+import ShowGrid from '../component/shows/ShowGrid';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
 
-  return <div> Starred page,starred {starredShows.length} </div>;
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: async () =>
+      getShowByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: 'false',
+  });
+  if (starredShows?.length === 0) {
+    return <div>NO Shows are starred</div>;
+  }
+
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+
+  if (starredShowsError) {
+    return <div>Error Occoured : {starredShowsError.message}</div>;
+  }
+
+  return <div> shows are loading ...! </div>;
 };
 
 export default Starred;
